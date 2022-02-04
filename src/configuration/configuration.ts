42d480063a8488a15caf1f064c4c901f389b87df
Andrés Carvajal <causticrez@MacBook-Pro-de-Andres.local>
2022-02-04 17:40:26 -0300

    first commit

---
+++ src/configuration/configuration.ts
@@ -0,0 +1,7 @@
+export default {
+    DB: {
+        URI: process.env.MONGODB_URI || "mongodb://localhost/condo",
+        USER: process.env.MONGODB_USER,
+        PASSWORD: process.env.MONGODB_PASS
+    }
+}
---
+++ src/controllers/users.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+export const getUsers = async (req: Request, res: Response) => {
+
+}
+
+export const createUser = async (req: Request, res: Response) => {
+
+}
